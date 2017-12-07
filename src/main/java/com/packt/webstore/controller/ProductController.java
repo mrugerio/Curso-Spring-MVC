61c91fbfaa6193ae958126441b3e529f6236ba6c
DESKTOP-7UL9EMA\cacao <mrugerio@gmail.com>
2017-12-07 15:13:43 -0600

    adding an exception handler. Pag.150

--- src/main/java/com/packt/webstore/controller/ProductController.java
+++ src/main/java/com/packt/webstore/controller/ProductController.java
@@ -12,6 +12,7 @@ import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.MatrixVariable;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -20,9 +21,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.packt.webstore.domain.Product;
 import com.packt.webstore.exception.NoProductsFoundUnderCategoryException;
+import com.packt.webstore.exception.ProductNotFoundException;
 import com.packt.webstore.service.ProductService;
 
 @Controller
@@ -110,6 +113,16 @@ public class ProductController {
 	            "unitsInStock",
 	            "condition",
    				"productImage");
+	}
+	
+	@ExceptionHandler(ProductNotFoundException.class)
+	public ModelAndView handleError(HttpServletRequest req, ProductNotFoundException exception) {
+	    ModelAndView mav = new ModelAndView();
+	    mav.addObject("invalidProductId", exception.getProductId());
+	    mav.addObject("exception", exception);
+	    mav.addObject("url", req.getRequestURL()+"?"+req.getQueryString());
+	    mav.setViewName("productNotFound");
+	    return mav;
 	}
 	
 }
--- src/main/java/com/packt/webstore/domain/repository/impl/InMemoryProductRepository.java
+++ src/main/java/com/packt/webstore/domain/repository/impl/InMemoryProductRepository.java
@@ -7,12 +7,14 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import com.packt.webstore.domain.Product;
 import com.packt.webstore.domain.repository.ProductRepository;
+import com.packt.webstore.exception.ProductNotFoundException;
 
 @Repository
 public class InMemoryProductRepository implements ProductRepository{
@@ -73,11 +75,15 @@ public class InMemoryProductRepository implements ProductRepository{
    
    @Override
    public Product getProductById(String productID) {
-      String SQL = "SELECT * FROM PRODUCTS WHERE ID = :id";
-      Map<String, Object> params = new HashMap<String, Object>();
-      params.put("id", productID);
-
-      return jdbcTemplate.queryForObject(SQL, params, new ProductMapper());
+      String SQL = "SELECT * FROM PRODUCTS WHERE ID = :id";  
+      Map<String, Object> params = new HashMap<>();
+      params.put("id", productID);  
+         
+      try {
+         return jdbcTemplate.queryForObject(SQL, params, new ProductMapper());
+      } catch (DataAccessException e) {
+         throw new ProductNotFoundException(productID);
+      }   
    }
    
    @Override
