92c16d8363c570bf6d94edf191835076ec908f3b
DESKTOP-7UL9EMA\cacao <mrugerio@gmail.com>
2017-12-06 21:15:41 -0600

    Time for action – serving static resources. Pag.131

--- src/main/java/com/packt/webstore/config/WebApplicationContextConfig.java
+++ src/main/java/com/packt/webstore/config/WebApplicationContextConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
@@ -48,5 +49,11 @@ public class WebApplicationContextConfig extends WebMvcConfigurerAdapter {
        return resource;    
     }
 	
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry
+    registry) {
+    registry.addResourceHandler("/img/**")
+    .addResourceLocations("/resources/images/");
+    }
     
 }
