50a2c46724b6ab840eb0157b431e1a4ce0267ece
DESKTOP-7UL9EMA\cacao <mrugerio@gmail.com>
2017-12-08 16:09:35 -0600

    adding Spring validation. Pag. 201

--- src/main/java/com/packt/webstore/controller/ProductController.java
+++ src/main/java/com/packt/webstore/controller/ProductController.java
@@ -28,6 +28,8 @@ import com.packt.webstore.domain.Product;
 import com.packt.webstore.exception.NoProductsFoundUnderCategoryException;
 import com.packt.webstore.exception.ProductNotFoundException;
 import com.packt.webstore.service.ProductService;
+import com.packt.webstore.validator.ProductValidator;
+import com.packt.webstore.validator.UnitsInStockValidator;
 
 @Controller
 @RequestMapping("market")
@@ -35,6 +37,9 @@ public class ProductController {
 	
 	@Autowired
 	private ProductService productService;
+	
+	@Autowired
+	private UnitsInStockValidator unitsInStockValidator;
 
 	@RequestMapping("/products")
 	public String list(Model model) {
@@ -109,7 +114,8 @@ public class ProductController {
 	
 	@InitBinder
 	public void initialiseBinder(WebDataBinder binder) {
-	   binder.setAllowedFields("productId",
+		binder.setValidator(unitsInStockValidator);		
+		binder.setAllowedFields("productId",
 	            "name",
 	            "unitPrice",
 	            "description",
